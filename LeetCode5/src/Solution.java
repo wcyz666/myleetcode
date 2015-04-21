ecbd14071e59daea2610c6d2a7fb61c6db8d2979
Wang Cheng <wcyz666@126.com>
2015-04-21 21:24:16 +0800

    Leetcode 5, medium

--- LeetCode5/src/Solution.java
+++ LeetCode5/src/Solution.java
@@ -2,43 +2,63 @@
 
 public class Solution {
     public String longestPalindrome(String s) {
-    	if (s.length() <= 2)
-    		return s;
     	
-    	String s1 = new StringBuilder(s).reverse().toString();
-    	if (s.equals(s1))
-    		return s;
-    	int length = s.length();
-    	int[][] b = new int[length][length];
-    	int subLength = Integer.MIN_VALUE;
+/* Manacher's algorithm   	
+ * 
+ * def longestPalindrome(self, s):
+    	    #The main idea is  manacher's algorithm
+    	    string="#"+"#".join(s)+"#"      # a trick that simplifies the problems
+    	                                    # e.g.:"aaa" ->"#a#a#a#" or "aa"->"#a#a#"
+    	    i=0
+    	    mxBorder=0                #stores the max Border that has been reached
+    	    mxCenter=0                # ------mxCenter------mxBorder
+    	    p=[0]*len(string)
+    	    res=[0,0]
+
+    	    while i< len(string):
+    	        if mxBorder>i:            #------mxCenter---i--mxBorder
+    	            p[i]=min(p[2*mxCenter-i],mxBorder-i) # pickes the min in(center to i or i to border) 
+
+    	        else:
+    	            p[i]=1
+
+    	        while i-p[i]>=0 and i+p[i]<len(string)and string[i-p[i]]==string[i+p[i]]:
+    	            p[i]+=1
+
+    	        if mxBorder<p[i]+i:
+    	            mxBorder=p[i]+i
+    	            mxCenter=i
+    	            if mxBorder-mxCenter>res[1]-res[0]:
+    	                res=[mxCenter,mxBorder]#records the temp maxCenter and mxBorder
+
+    	        i+=1
+
+    	    return "".join([x for x in string[2*res[0]-res[1]+1:res[1]] if x!='#'])*/
     	
-    	for (int i = 1; i < length; i++){
-    		for (int j = 1; j < length; j++){
-    			if (s1.charAt(i) == s.charAt(j)){
-    				b[i][j] = b[i-1][j-1] + 1;
-    				if (b[i][j] > subLength)
-    					subLength = b[i][j];
-    			}    				
-    			else
-    				b[i][j] = 0;
-    		}
-    	}
-    	for (int i = length - 1; i >= subLength - 1; i--)
-    		for (int j = length - 1; j >= subLength - 1; j--)
-    			if (b[i][j] == subLength){
-    				StringBuilder sBuilder = new StringBuilder();
-    				while (subLength > 0){
-    					sBuilder.append(s.charAt(j--));
-    					subLength--;
-    				}
-    				return sBuilder.toString();
-    			}
-    				
-    	return null;
     	
+        int maxLength = 1, start = 0;
+        for (int i = 1; i < s.length() - 1; i++){
+            int j = i - 1, k = i + 1;
+            while (j >= 0 && k < s.length() && s.charAt(k) == s.charAt(j)){
+                j--; k++;
+            }
+            if (k - j - 1 > maxLength){
+                start = j + 1;
+                maxLength = k - j - 1;
+            }
+        }
+        for (int i = 0; i < s.length() - 1; i++){
+        	if (s.charAt(i) != s.charAt(i + 1)) continue;
+            int j = i - 1, k = i + 2;
+            while (j >= 0 && k < s.length() && s.charAt(k) == s.charAt(j)){
+                j--; k++;
+            }
+            if (k - j - 1 > maxLength){
+                start = j + 1;
+                maxLength = k - j - 1;
+            }
+        }
+        return s.substring(start, start + maxLength);     
     }
-    
-    public static void main(String[] args) {
-		System.out.println(new Solution().longestPalindrome("characteristic"));
-	}
+
 }
