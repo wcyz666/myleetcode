485ac9e9b9c7dcf54e8e53b4e0f8ac9f271a1d47
Wang Cheng <wcyz666@126.com>
2015-05-15 10:17:14 +0800

    LeetCode 116, upgraded

--- LeetCode116/src/Solution.java
+++ LeetCode116/src/Solution.java
@@ -8,7 +8,20 @@ import java.util.Queue;
       TreeLinkNode(int x) { val = x; }
   }
 
-
+/* void connect(TreeLinkNode *root) {
+	    if (root == NULL) return;
+	    TreeLinkNode *pre = root;
+	    TreeLinkNode *cur = NULL;
+	    while(pre->left) {
+	        cur = pre;
+	        while(cur) {
+	            cur->left->next = cur->right;
+	            if(cur->next) cur->right->next = cur->next->left;
+	            cur = cur->next;
+	        }
+	        pre = pre->left;
+	    }
+	}*/
 public class Solution {
     public void connect(TreeLinkNode root) {
         
