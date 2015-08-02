cd8f9a90e1578e0034debe81779a824870f659a9
Wang Cheng <wcyz666@126.com>
2015-08-02 16:43:42 +0800

    LeetCode 237, easy

---
+++ LeetCode237/src/Solution.java
@@ -0,0 +1,12 @@
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+
+public class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
